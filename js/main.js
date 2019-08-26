0ad7ce9b2f0ec51d89b816400d59bfb6792118c7
brian ng <brianngchongeu@gmail.com>
2019-08-26 20:53:45 +1000

    clean up

--- js/main.js
+++ js/main.js
@@ -1,21 +1,41 @@
+// jquery
 var jquery = require("jquery");
-window.$ = window.jQuery = jquery; // notice the definition of global variables here
+window.$ = window.jQuery = jquery;
 
+// momentjs
 var moment = require("moment");
 
-function makeTimer() {
-  var eventTime = moment('16 September 2019 00:00:00 +1000','DD MMMM YYYY hh:mm:ss Z');
-  var currentTime = moment();
-  var diffTime = eventTime - currentTime;
-  var duration = moment.duration(diffTime);
+// define end date
+var endDate = "6 September 2019 00:00:00 +1000";
+
+// setup countdown timer
+function setupCountdown() {
+  let eventTime = moment(endDate, "DD MMMM YYYY hh:mm:ss Z");
+  let currentTime = moment();
+  let diffTime = eventTime - currentTime;
+  let duration = moment.duration(diffTime);
 
   $("#days").html(duration.days() + "<span>Days</span>");
   $("#hours").html(duration.hours() + "<span>Hours</span>");
   $("#minutes").html(duration.minutes() + "<span>Minutes</span>");
   $("#seconds").html(duration.seconds() + "<span>Seconds</span>");
+
+  // console.log(
+  //   duration.days() +
+  //     " | " +
+  //     duration.hours() +
+  //     " | " +
+  //     duration.minutes() +
+  //     " | " +
+  //     duration.seconds()
+  // );
 }
 
-// per second tick
-setInterval(function() {
-  makeTimer();
-}, 1000);
+function triggerCountdown() {
+  setInterval(function() {
+    setupCountdown();
+  }, 1000);
+}
+
+// console.log(endDate);
+triggerCountdown();
