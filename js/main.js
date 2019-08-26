e79995a665c55968ed0fdd0ce9c4d6dad77fdc16
brian ng <brianngchongeu@gmail.com>
2019-08-26 21:22:30 +1000

    updated changes
    - installed new packages
    - using normalize.css
    - 'use strict' mode turned on
    - cleanup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+"use strict"; //https://www.w3schools.com/js/js_strict.asp
+
+// normalize.css
+import "normalize.css";
+
 // jquery
 var jquery = require("jquery");
 window.$ = window.jQuery = jquery;
@@ -5,8 +10,12 @@ window.$ = window.jQuery = jquery;
 // momentjs
 var moment = require("moment");
 
-// define end date
-var endDate = "6 September 2019 00:00:00 +1000";
+// global variables
+const endDate = "6 September 2019 00:00:00 +1000";
+const timerDays = "#timer-days";
+const timerHours = "#timer-hours";
+const timerMinutes = "#timer-minutes";
+const timerSeconds = "#timer-seconds";
 
 // setup countdown timer
 function setupCountdown() {
@@ -15,10 +24,10 @@ function setupCountdown() {
   let diffTime = eventTime - currentTime;
   let duration = moment.duration(diffTime);
 
-  $("#days").html(duration.days() + "<span>Days</span>");
-  $("#hours").html(duration.hours() + "<span>Hours</span>");
-  $("#minutes").html(duration.minutes() + "<span>Minutes</span>");
-  $("#seconds").html(duration.seconds() + "<span>Seconds</span>");
+  $(timerDays).html(duration.days() + "<span>Days</span>");
+  $(timerHours).html(duration.hours() + "<span>Hours</span>");
+  $(timerMinutes).html(duration.minutes() + "<span>Minutes</span>");
+  $(timerSeconds).html(duration.seconds() + "<span>Seconds</span>");
 
   // console.log(
   //   duration.days() +
