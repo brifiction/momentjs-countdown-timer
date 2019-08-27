3e210bb262e1925df3db4a014d70104d2d1ec4bc
brian ng <brianngchongeu@gmail.com>
2019-08-28 08:30:15 +1000

    minor cleanup

--- src/js/components/countdown.js
+++ src/js/components/countdown.js
@@ -5,8 +5,8 @@ class Countdown {
 
     constructor(endDate, timerDays, timerHours, timerMinutes, timerSeconds) {
         this.endDate = endDate;
-        // this.timerMonths = timerMonths;
-        // this.timerWeeks = timerWeeks;
+        // this.timerMonths = timerMonths; // if needed
+        // this.timerWeeks = timerWeeks; // if needed
         this.timerDays = timerDays;
         this.timerHours = timerHours;
         this.timerMinutes = timerMinutes;
@@ -14,37 +14,27 @@ class Countdown {
     }
 
     setupCountdown() {
+        // define fields
         const eventTime = moment(this.endDate, "DD-MM-YYYY hh:mm:ss Z");
         const currentTime = moment();
         const diffTime = eventTime.diff(currentTime);
         const duration = moment.duration(diffTime);
 
+        // primary time unit used. for example, days.
         const days = eventTime.diff(currentTime, 'days'); // = duration.asDays().toFixed(0)
 
+        // change / update usage. for example, from asDays() to days() if not primary
         // $(this.timerMonths).html(duration.asMonths().toFixed(0) + "<span>Months</span>");
-        // $(this.timerWeeks).html(duration.weeks() + "<span>Days</span>");
-
+        // $(this.timerWeeks).html(duration.asWeeks().toFixed(0) + "<span>Weeks</span>");
         $(this.timerDays).html(duration.asDays().toFixed(0) + "<span>Days</span>");
         $(this.timerHours).html(duration.hours() + "<span>Hours</span>");
         $(this.timerMinutes).html(duration.minutes() + "<span>Minutes</span>");
         $(this.timerSeconds).html(duration.seconds() + "<span>Seconds</span>");
-
-        // console.log(currentTime);
-        // console.log(
-        //     duration.days() +
-        //     " | " +
-        //     duration.hours() +
-        //     " | " +
-        //     duration.minutes() +
-        //     " | " +
-        //     duration.seconds()
-        // );
     }
 
     triggerCountdown() {
         //starts the interval function
         var self = this;
-
         // per second tick
         this.timer = setInterval(function () {
             self.setupCountdown();
@@ -53,35 +43,3 @@ class Countdown {
 }
 
 export { Countdown };
-
-// // global variables
-// const vars = {
-//     endDate: "6 September 2019 00:00:00 +1000",
-//     timerDays: "#timer-days",
-//     timerHours: "#timer-hours",
-//     timerMinutes: "#timer-minutes",
-//     timerSeconds: "#timer-seconds"
-// };
-
-// // setup countdown timer
-// function setupCountdown() {
-//     let eventTime = moment(vars.endDate, "DD MMMM YYYY hh:mm:ss Z");
-//     let currentTime = moment();
-//     let diffTime = eventTime - currentTime;
-//     let duration = moment.duration(diffTime);
-
-//     $(vars.timerDays).html(duration.days() + "<span>Days</span>");
-//     $(vars.timerHours).html(duration.hours() + "<span>Hours</span>");
-//     $(vars.timerMinutes).html(duration.minutes() + "<span>Minutes</span>");
-//     $(vars.timerSeconds).html(duration.seconds() + "<span>Seconds</span>");
-// }
-
-// // trigger countdown timer (per second = 1000)
-// function triggerCountdown() {
-//     // per second tick
-//     setInterval(function () {
-//         setupCountdown();
-//     }, 1000);
-// }
-
-// export { triggerCountdown };
